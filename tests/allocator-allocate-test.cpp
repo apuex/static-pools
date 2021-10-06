9f249ce3bdff293d0d3418dc86f2b0d8e847a086
Apuex <32790528+apuex@users.noreply.github.com>
2021-10-06 11:45:50 +0800

    add basic allocate flow test.

---
+++ tests/allocator-allocate-test.cpp
@@ -0,0 +1,17 @@
+#include "allocator.hpp"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace apuex;
+
+typedef apuex::allocator<uint32_t> myallocator;
+
+int main(int argc, char* argv[]) {
+  uint32_t d = 0;
+  myallocator myalloc(32);
+  vector<uint32_t, myallocator> v(1, d, myalloc);
+
+  return 0;
+}
+
