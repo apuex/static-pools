1e0be4bfa48199576ec9d6b76c7d3a44ce62c7a3
Wangxy <xtwxy@hotmail.com>
2021-11-23 14:03:38 +0800

    change to statically linking CRT

--- build-vc6.bat
+++ build-vc6.bat
@@ -1,5 +1,5 @@
 cmake -G "NMake Makefiles" ^
- -DCMAKE_CXX_FLAGS="/MD /D_WIN32_WINNT=0x0502" ^
+ -DCMAKE_CXX_FLAGS="/MT /D_WIN32_WINNT=0x0502" ^
  -DCMAKE_CXX_FLAGS_RELEASE="/O2 /DNDEBUG" ^
  -DCMAKE_EXE_LINKER_FLAGS="/machine:IX86 /subsystem:windows,5.02 /version:5.2" ^
  -DCMAKE_MODULE_LINKER_FLAGS="/machine:IX86 /subsystem:windows,5.02" ^
